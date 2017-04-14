c8d37e34440bf0b1eafe57e89d09705f76449583
Alistair Amick <aamickWork@tutamail.com>
2017-04-14 17:36:38 -0400

    First update

--- setup.py
+++ setup.py
@@ -6,16 +6,15 @@ except ImportError:
 config = {
     'description': 'Quizzes the user on their knowledge of Chinese',
     'author': 'Alistair Amick',
-    'url': '',
-    'download_url': '',
+    'url': 'https://github.com/AlistairAmick/Chinese-Language-Quizzer',
     'author_email': 'aamickWork@tutamail.com',
-    'version': '0.1',
+    'version': '1.1.1',
     'install_requires': ['nose'],
     'packages': ['language_game'],
     'scripts': ['language_game/main.py', 'language_game/score.py',
                 'language_game/rungame.py',	'language_game/lessonarchive.py',
                 'language_game/lesson1.py', 'language_game/lesson2.py', 'language_game/lesson3.py',],
-    'name': 'Chinese Language Game'
+    'name': 'Chinese-Language-Quizzer'
 }
 
 setup(**config)
